aa72fc0f2f52072ec8d6ced31ff2b5bc5efddcd3
Kübra KARAMANLI <56437490+kubrakaramanli@users.noreply.github.com>
2021-05-01 01:52:28 +0300

    Add files via upload

---
+++ javacamp/hw3/CourseManager.java
@@ -0,0 +1,14 @@
+package hw3;
+
+public class CourseManager {
+	public void register (Course course) {
+		System.out.println(course.courseName + " You registered in the course");
+		
+	}
+
+	public void rollCall (Course course) {
+		System.out.println(course.courseName + " Your roll call has been received ");
+		
+		
+	}
+}
---
+++ javacamp/hw3/Students.java
@@ -0,0 +1,15 @@
+package hw3;
+
+public class Students extends User {
+	
+	String courseName;
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+}
